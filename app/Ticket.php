80f67b441f5f3987f95e099c66cf183c1b09e8e0
Robin Håkansson <Dailyman@users.noreply.github.com>
2018-05-29 08:27:01 +0200

    Added relationships

--- app/Ticket.php
+++ app/Ticket.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Ticket extends Model
 {
-    //
+    public function cinema()
+    {
+        return $this->belongsTo('App\Cinema');
+    }
+
+    public function movie()
+    {
+        return $this->belongsTo('App\Movie');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
 }
