9dc3b14338be13489598a28b3adeff45e5fd809f
Robin Håkansson <Dailyman@users.noreply.github.com>
2018-05-30 11:19:18 +0200

    Added database seeds to run

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $this->call([
+            CinemaTableSeeder::class,
+            TicketTableSeeder::class,
+            MovieTableSeeder::class,
+        ]);
     }
 }
