0814ac760cd6ff688b8f56555bc1afd3befc6ac6
Oskar Katra <oskarkatra@oskars-mbp.clients.internal.mah.se>
2018-05-30 11:20:30 +0200

    seeds added

---
+++ database/seeds/CinemaSeeder.php
@@ -0,0 +1,47 @@
+<?php
+DB::table('cinemas')->insert([
+    [
+        "name" => "Filmstaden Heron City",
+        "seats" => "200",
+    ],
+    [
+        "name" => "Filmstaden Kista",
+        "seats" => "330",
+    ],
+    [
+        "name" => "Filmstaden Råsunda",
+        "seats" => "220",
+    ],
+    [
+        "name" => "Filmstaden Sickla",
+        "seats" => "330",
+    ],
+    [
+        "name" => "Filmstaden Söder",
+        "seats" => "170",
+    ],
+    [
+        "name" => "Filmstaden Täby",
+        "seats" => "220",
+    ],
+    [
+        "name" => "Rigoletto",
+        "seats" => "300",
+    ],
+    [
+        "name" => "Filmstaden Sergel",
+        "seats" => "200",
+    ],
+    [
+        "name" => "Filmstaden Vällingby",
+        "seats" => "100",
+    ],
+    [
+        "name" => "Filmstaden Scandinavia",
+        "seats" => "200",
+    ],
+    [
+        "name" => "Grand Lindingö",
+        "seats" => "200",
+    ],
+]);
---
+++ database/seeds/MovieSeeder.php
@@ -0,0 +1,48 @@
+
+<?php
+DB::table('movies')->insert([
+    [
+        "name" => "Solo: A Star Wars Story",
+        "duration" => "135",
+    ],
+    [
+        "name" => "A Quiet Place",
+        "duration" => "91",
+    ],
+    [
+        "name" => "Darkest Hour",
+        "duration" => "125",
+    ],
+    [
+        "name" => "Deadpool 2",
+        "duration" => "119",
+    ],
+    [
+        "name" => "Isle of Dogs",
+        "duration" => "101",
+    ],
+    [
+        "name" => "Tomb Raider",
+        "duration" => "117",
+    ],
+    [
+        "name" => "The Post",
+        "duration" => "116",
+    ],
+    [
+        "name" => "Black Panther",
+        "duration" => "134",
+    ],
+    [
+        "name" => "Ghostland",
+        "duration" => "91",
+    ],
+    [
+        "name" => "Good Bye Christopher Robin",
+        "duration" => "107",
+    ],
+    [
+        "name" => "Jumanji",
+        "duration" => "119",
+    ],
+]);
