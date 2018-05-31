1e343a0e838750457ae92174766ca06a60a036d8
Robin Håkansson <Dailyman@users.noreply.github.com>
2018-05-31 15:27:39 +0200

    Trying to test

--- tests/CinemaTest.php
+++ tests/CinemaTest.php
@@ -13,19 +13,40 @@ require dirname(__DIR__) . '/app/Http/Controllers/CinemaController.php';
 
 final class CinemaTest extends TestCase
 {
-    public function testStoreFromValidInput()
-        {   
-            $requested_arr = [
-                'name' => 'FilmstadenTest',
-                'seats' => 100
-            ];
-            $controller = new CinemaController();
-            $response = $this->$controller->store(new Request ($requested_arr));
-            $data = json_decode($response ->getContent(), true);
-            if ($data['status']) {
-                $this->assertTrue(true);
-            } else {
-                $this->assertTrue(false);
-            }
-        }
+    private $controller;
+
+    protected function setUp()
+    {
+        $this->controller  = new CinemaController();
     }
+
+    protected function tearDown()
+    {
+        $this->controller  = NULL;
+    }
+
+    // public function testStoreFromValidInput($name, $seats)
+    public function testStoreFromValidInput()
+    {
+        parent::setUp();
+        $requested_arr = [
+            // 'name' => $name,
+            // 'seats' => $seats
+            'name' => 'FilmstadenTest',
+            'seats' => 100
+        ];
+        // $controller = new CinemaController();
+        // $response = $this->controller->store(new Request ($requested_arr));
+        // $data = json_decode($response->getContent(), true);
+        // if ($data['status']) {
+        //     $this->assertTrue(true);
+        // } else {
+        //     $this->assertTrue(false);
+        // }
+        // $response->assertRedirect(route('cinemas.index'));
+
+        $response = $this->followingRedirects()
+        ->post('/cinema/store', $requested_arr)
+        ->assertStatus(200);
+    }
+}
