bf4e1ae8ac25341379afab4454e69d50cfe3600a
robinhakansson <robin.lars.hakansson@gmail.com>
2018-06-05 12:34:52 +0200

    Made better structure for cinema views

--- resources/views/cinemas/edit.blade.php
+++ resources/views/cinemas/edit.blade.php
@@ -1,21 +1,36 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Uppdatera biosalong</h1>
+
 
     <form class="" action="{{ route('cinemas.update', $cinema->id) }}" method="post">
         @method('PUT')
         @csrf
-        <div class="form-group">
+        <div class="form-group row">
+          <div class="col-md-12">
+            <h1>Uppdatera biosalong</h1>
+          </div>
+        </div>
+        <div class="form-group row">
+          <div class="col-md-12">
             <h2>ID {{ $cinema->id }}</h2>
+          </div>
         </div>
-        <div class="form-group">
-            <label for="">Salongnamn</label>
-            <input type="text" step="any" name="name" value="{{ $cinema->name }}">
+        <div class="form-group row">
+          <div class="col-md-2">
+            <label for="">Salongnamn:</label>
+          </div>
+          <div class="col-md-10">
+              <input type="text" step="any" name="name" value="{{ $cinema->name }}" required>
+          </div>
         </div>
-        <div class="form-group">
-            <label for="">Antal platser</label>
-            <input type="number" step="any" name="seats" value="{{ $cinema->seats }}">
+        <div class="form-group row">
+          <div class="col-md-2">
+            <label for="">Antal platser:</label>
+          </div>
+          <div class="col-md-10">
+            <input type="number" step="any" name="seats" value="{{ $cinema->seats }}" required>
+          </div>
         </div>
         <input button class="button updatebutton" type="submit" name="submit" value="Uppdatera"></button>
     </form>
