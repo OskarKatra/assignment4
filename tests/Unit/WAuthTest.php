4b8ecfa635a4361d08c9a11c62632bb46c2ace62
robinhakansson <robin.lars.hakansson@gmail.com>
2018-06-09 02:24:53 +0200

    Updated and added more tests

--- tests/Unit/WAuthTest.php
+++ tests/Unit/WAuthTest.php
@@ -31,7 +31,7 @@ class WAuthTest extends TestCase
        $response = $this->call('GET', '/cinemas/create');
        $this->assertEquals(200, $response->status());
      }
-     // --------------------------------------------------------------------------
+     // ------------------------------------------------------------------------
      public function test_guest_access_to_cinema_edit_page()
      {
        $response = $this->call('GET', '/cinemas/1/edit');
@@ -66,7 +66,6 @@ class WAuthTest extends TestCase
             '_token' => csrf_token()
           ]);
        $this->assertEquals(302, $response->status());
-       // $response->assertRedirect('cinemas/1');
        $response->assertRedirect('cinemas');
      }
      // ------------------------------------------------------------------------
@@ -113,36 +112,111 @@ class WAuthTest extends TestCase
      }
      // ------------------------------------------------------------------------
 
+
+
+
+
      /**
       * Testing auth pages for movie.
       */
-     public function test_guest_access_to_movie_create_page()
-     {
-      $response = $this->call('GET', '/movies/create');
-      $this->assertEquals(302, $response->status());
-      $response->assertRedirect('login');
-     }
-
-     public function test_authenticated_user_access_to_movie_create_page()
-     {
-       $this->actingAs(factory('App\User')->make());
+      public function test_guest_access_to_movie_create_page()
+      {
        $response = $this->call('GET', '/movies/create');
-       $this->assertEquals(200, $response->status());
-     }
-
-     public function test_guest_access_to_movie_edit_page()
-     {
-       $response = $this->call('GET', '/movies/1/edit');
        $this->assertEquals(302, $response->status());
        $response->assertRedirect('login');
-     }
+      }
 
-     public function test_authenticated_user_access_to_movie_edit_page()
-     {
-       $this->actingAs(factory('App\User')->make());
-       $response = $this->call('GET', '/movies/1/edit');
-       $this->assertEquals(200, $response->status());
-     }
+      public function test_authenticated_user_access_to_movie_create_page()
+      {
+        $this->actingAs(factory('App\User')->make());
+        $response = $this->call('GET', '/movies/create');
+        $this->assertEquals(200, $response->status());
+      }
+      // -----------------------------------------------------------------------
+      public function test_guest_access_to_movie_edit_page()
+      {
+        $response = $this->call('GET', '/movies/1/edit');
+        $this->assertEquals(302, $response->status());
+        $response->assertRedirect('login');
+      }
+
+      public function test_authenticated_user_access_to_movie_edit_page()
+      {
+        $this->actingAs(factory('App\User')->make());
+        $response = $this->call('GET', '/movies/1/edit');
+        $this->assertEquals(200, $response->status());
+      }
+      // -----------------------------------------------------------------------
+      public function test_guest_access_to_movie_store()
+      {
+        $response = $this->call('POST', '/movies', [
+             'title' => 'random',
+             'genre' => 'random',
+             'runtime' => 120,
+             '_token' => csrf_token()
+           ]);
+        $this->assertEquals(302, $response->status());
+        $response->assertRedirect('login');
+      }
+
+      public function test_authenticated_user_access_to_movie_store()
+      {
+        $this->actingAs(factory('App\User')->make());
+        $response = $this->call('POST', '/movies', [
+             'title' => 'random',
+             'genre' => 'random',
+             'runtime' => 120,
+             '_token' => csrf_token()
+           ]);
+        $this->assertEquals(302, $response->status());
+        $response->assertRedirect('movies');
+      }
+      // -----------------------------------------------------------------------
+      public function test_guest_access_to_movie_update()
+      {
+        $response = $this->call('PUT', '/movies/1', [
+          'title' => 'random',
+          'genre' => 'random',
+          'runtime' => 120,
+             '_token' => csrf_token()
+           ]);
+        $this->assertEquals(302, $response->status());
+        $response->assertRedirect('login');
+      }
+
+      public function test_authenticated_user_access_to_movie_update()
+      {
+        $this->actingAs(factory('App\User')->make());
+        $response = $this->call('PUT', '/movies/1', [
+          'title' => 'random',
+          'genre' => 'random',
+          'runtime' => 120,
+             '_token' => csrf_token()
+           ]);
+        $this->assertEquals(302, $response->status());
+        $response->assertRedirect('movies/1');
+      }
+      // -----------------------------------------------------------------------
+      public function test_guest_access_to_movie_destroy()
+      {
+        $response = $this->call('DELETE', '/movies/1', [
+             '_token' => csrf_token()
+           ]);
+        $this->assertEquals(302, $response->status());
+        $response->assertRedirect('login');
+      }
+
+      public function test_authenticated_user_access_to_movie_destroy()
+      {
+        $this->actingAs(factory('App\User')->make());
+        $response = $this->call('DELETE', '/movies/1', [
+             '_token' => csrf_token(),
+           ]);
+        $this->assertEquals(302, $response->status());
+        $response->assertRedirect('movies');
+      }
+      // -----------------------------------------------------------------------
+
 
 
 
@@ -162,7 +236,7 @@ class WAuthTest extends TestCase
        $response = $this->call('GET', '/tickets/create');
        $this->assertEquals(200, $response->status());
      }
-
+     // ------------------------------------------------------------------------
      public function test_guest_access_to_ticket_edit_page()
      {
        $response = $this->call('GET', '/tickets/1/edit');
@@ -176,6 +250,102 @@ class WAuthTest extends TestCase
        $response = $this->call('GET', '/tickets/1/edit');
        $this->assertEquals(200, $response->status());
      }
+     // ------------------------------------------------------------------------
+     public function test_guest_access_to_ticket_store()
+     {
+       $response = $this->call('POST', '/tickets', [
+         'time' => '2018-08-28 16:15:00',
+         'seat' => 1,
+         'price' => 120,
+         'cinema_id' => 1,
+         'movie_id' => 1,
+         'user_id' => null,
+            '_token' => csrf_token()
+          ]);
+       $this->assertEquals(302, $response->status());
+       $response->assertRedirect('login');
+     }
+
+     public function test_authenticated_user_access_to_ticket_store()
+     {
+       $this->actingAs(factory('App\User')->make());
+       $response = $this->call('POST', '/tickets', [
+         'time' => '2018-08-28 16:15:00',
+         'seat' => 1,
+         'price' => 120,
+         'cinema_id' => 1,
+         'movie_id' => 1,
+         'user_id' => null,
+            '_token' => csrf_token()
+          ]);
+       $this->assertEquals(302, $response->status());
+       $response->assertRedirect('tickets');
+     }
+     // ------------------------------------------------------------------------
+     public function test_guest_access_to_ticket_update()
+     {
+       $response = $this->call('PUT', '/tickets/1', [
+         'time' => '2018-08-28 16:15:00',
+         'seat' => 1,
+         'price' => 120,
+         'cinema_id' => 1,
+         'movie_id' => 1,
+         'user_id' => null,
+            '_token' => csrf_token()
+          ]);
+       $this->assertEquals(302, $response->status());
+       $response->assertRedirect('login');
+     }
+
+     public function test_authenticated_user_access_to_ticket_update()
+     {
+       $this->actingAs(factory('App\User')->make());
+       $response = $this->call('PUT', '/tickets/1', [
+         'time' => '2018-08-28 16:15:00',
+         'seat' => 1,
+         'price' => 120,
+         'cinema_id' => 1,
+         'movie_id' => 1,
+         'user_id' => null,
+            '_token' => csrf_token()
+          ]);
+       $this->assertEquals(302, $response->status());
+       $response->assertRedirect('tickets/1');
+     }
+     // ------------------------------------------------------------------------
+     public function test_guest_access_to_ticket_destroy()
+     {
+       $response = $this->call('DELETE', '/tickets/1', [
+            '_token' => csrf_token()
+          ]);
+       $this->assertEquals(302, $response->status());
+       $response->assertRedirect('login');
+     }
+
+     public function test_authenticated_user_access_to_ticket_destroy()
+     {
+       $this->actingAs(factory('App\User')->make());
+       $response = $this->call('DELETE', '/tickets/1', [
+            '_token' => csrf_token(),
+          ]);
+       $this->assertEquals(302, $response->status());
+       $response->assertRedirect('tickets');
+     }
+     // ------------------------------------------------------------------------
+     public function test_guest_access_to_show_my_tickets_page()
+     {
+      $response = $this->call('GET', '/myTickets');
+      $this->assertEquals(302, $response->status());
+      $response->assertRedirect('login');
+     }
+
+     public function test_authenticated_user_access_to_show_my_tickets_page()
+     {
+       $this->actingAs(factory('App\User')->make());
+       $response = $this->call('GET', '/myTickets');
+       $this->assertEquals(200, $response->status());
+     }
+     // ------------------------------------------------------------------------
 
 
 
